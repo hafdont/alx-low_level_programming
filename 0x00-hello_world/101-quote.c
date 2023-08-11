1971a32f48150881207a6540630494620e2564d7
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-11 09:57:09 -0700

    Gospel of C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
-#include <unnistd.h>
+#include <unistd.h>
+
+
 /**
  * main - Entry point
  *
- * Description using the puts function 
+ * Description using the puts function
  * Return: 0
  */
 int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n";
-	write(2, str1, 59)
+	write(2, str1, 59);
 		return (1);
 }

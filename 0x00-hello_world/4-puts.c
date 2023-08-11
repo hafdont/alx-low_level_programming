47f8670a657f95b9462dba907ffd1e9248a077c0
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-11 07:51:11 -0700

    Gospel of C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main entry point
+ * Description using the main fucntion
+ * Return: 0
+ */
+int main(void)
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}

84d0e27e129600ebccd7b80bbfb8b245b10d3955
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-11 09:18:05 -0700

    Gospel of C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@
 int main(void)
 {
 	int nambari;
-	long int nambari_kikubwa;
+	long int nambari_kubwa;
 	long long int nambari_jikubwa;
 	char karakta;
 	float desimoli;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(karakta));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(nambari));
-	prinntf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(nambari_kubwa));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(nambari_kubwa));
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(nambari_jikubwa));
 	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(desimoli));
 	return (0);

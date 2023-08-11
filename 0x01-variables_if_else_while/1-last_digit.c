23292de34c603782dfeeadc3ef97179b6e8a9c58
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-11 11:19:09 -0700

    Gospel of C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,3 +5,20 @@
 /*
  * main - Entry point
  *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = reand() - RAND_MAX / 2;
+
+	if ((n % 10) == 0)
+		printf("The final digit of %d is %d and it hold the value of 0\n", n, n % 10);
+	else if ((n % 10) > 5)
+		printf("The last digit of %d is %d and it is greater than %\n", n, n % 10);
+	else
+	     printf("The ending of %d is %d and it less than 6 and not 0\n", n, n % 10);
+	return (0);
+}

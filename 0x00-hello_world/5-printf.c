a9000a8bc50d7260f29f300b20ff390d554c58e8
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-11 08:49:47 -0700

    Gospel of C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the entry point bruv
+ *
+ * Description: how to print using a print function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int nambari;
+	long int nambari_kikubwa;
+	long long int nambari_jikubwa;
+	char karakta;
+	float desimoli;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(karakta));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(nambari));
+	prinntf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(nambari_kubwa));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(nambari_jikubwa));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(desimoli));
+	return (0);
+}

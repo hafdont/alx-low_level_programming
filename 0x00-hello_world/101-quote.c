0c7ffccc4b1cf693eec6bdc74e910a3f0c8c4a20
Hafiz Masoud Yusuf <hafizmasoud7@gmail.com>
2023-08-13 20:20:37 +0300

    Gospel of C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,13 @@
  * main - Entry point
  *
  * Description using the puts function
- * Return: 0
+ * Return: 1 (failure)
  */
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - DoraKorpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+
 	write(2, str1, 59);
 		return (1);
 }

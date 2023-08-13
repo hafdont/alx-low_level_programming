1dd2a9fef6d394f348fae779e22a6781735cd838
Hafiz Masoud Yusuf <hafizmasoud7@gmail.com>
2023-08-13 21:51:20 +0300

    Gospel of C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-#include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*
+/**
  * main - Entry point
+ *
+ * Description: Assigns a random number to the variable n
+ * and prints whether its a postive,negative orzero
  *
  * Return: Always 0 (Success)
  */
@@ -12,13 +15,13 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = reand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) == 0)
-		printf("The final digit of %d is %d and it hold the value of 0\n", n, n % 10);
+		printf("The final digit of %d is %d and it has a value of 0\n", n, n % 10);
 	else if ((n % 10) > 5)
-		printf("The last digit of %d is %d and it is greater than %\n", n, n % 10);
+		printf("The last digit of %d is %d and it is greater than 5\n", n, n % 10);
 	else
-	     printf("The ending of %d is %d and it less than 6 and not 0\n", n, n % 10);
+	     printf("The ending of %d is %d and it ls than 6 and not 0\n", n, n % 10);
 	return (0);
 }

52edf7566fefd35c193d3868a47a2fd70575ee8e
General Account1 <ga1@office.local>
2023-08-17 08:12:28 +0300

    C is cool

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 ...
  */
 int main(void)
 {
-    int num;
-
-    for (num = 0; num <= 9; num++)
-    {
-        putchar(num + '0'); // Print the current digit
-        if (num < 9)
-        {
-            putchar(','); // Print comma
-            putchar(' '); // Print space
-        }
-    }
-    putchar('\n'); // Print newline
-
-    return (0);
+int q;
+for (q = 0; q <= 9; q++)
+{
+putchar((q % 10) + '0');
+if (q == 9)
+	continue;
+putchar(',');
+putchar(' ');
 }
-
+putchar('\n');
+return (0);
+}

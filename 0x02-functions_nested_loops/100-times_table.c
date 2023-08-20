4105bce6df3a709a043c1470909bc17103d80ed1
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-20 09:59:10 -0700

    Gospel of C

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,23 +9,38 @@
  */
 void print_times_table(int n)
 {
-    int i, j, result;
+	int j, k, op;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n; i++)
-        {
-            for (j = 0; j <= n; j++)
-            {
-                result = i * j;
-
-                if (j == 0)
-                    printf("%d", result);
-                else
-                    printf(", %3d", result);
-            }
-            printf("\n");
-        }
-    }
+	if (n >= 0 && n <= 15)
+	{
+	for (j = 0; j <= n; j++)
+	{
+	_putchar(48);
+	for (k = 1; k <= n; k++)
+	{
+		op = j * k;
+		_putchar(44);
+		_putchar(32);
+		if (op <= 9)
+		{
+		_putchar(32);
+		_putchar(32);
+		_putchar(op + 48);
+		}
+		else if (op <= 99)
+		{
+		_putchar(32);
+		_putchar((op / 10) + 48);
+		_putchar((op % 10) + 48);
+		}
+		else
+		{
+		_putchar(((op / 100) % 10) + 48);
+		_putchar(((op / 10) % 10) + 48);
+		_putchar((op % 10) + 48);
+		}
+	}
+	_putchar('\n');
+	}
+	}
 }
-

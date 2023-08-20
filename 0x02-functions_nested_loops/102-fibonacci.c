15732e67f39bb862a3b4da679adaaba47e9ab714
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-20 10:26:50 -0700

    Gospel of C

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,20 @@
  */
 int main(void)
 {
-    int n;
-    unsigned long long int fib1 = 1, fib2 = 2, next;
+	int n;
+	unsigned long long int fib1 = 1, fib2 = 2, next;
 
-    printf("%llu, %llu", fib1, fib2);
+	printf("%llu, %llu", fib1, fib2);
 
-    for (n = 2; n < 50; n++)
-    {
-        next = fib1 + fib2;
-        printf(", %llu", next);
-        fib1 = fib2;
-        fib2 = next;
-    }
+	for (n = 2; n < 50; n++)
+	{
+		next = fib1 + fib2;
+		printf(", %llu", next);
+		fib1 = fib2;
+		fib2 = next;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
-

2d7e7dd4d76721537af838067f50102cf8588e05
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-20 09:53:35 -0700

    I cant see my cat

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,26 +5,25 @@
  * print_to_98 - Prints all natural numbers from n to 98
  * @n: Starting number
  *
+ * Description: this function prints numbers from @n to 98
+ *
  * Return: void
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        while (n < 98)
-        {
-            printf("%d, ", n);
-            n++;
-        }
-    }
-    else
-    {
-        while (n > 98)
-        {
-            printf("%d, ", n);
-            n--;
-        }
-    }
-    printf("%d\n", n);
+	if (n > 98)
+	{
+	for (; n > 98; n--)
+		{
+		printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+		printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
-

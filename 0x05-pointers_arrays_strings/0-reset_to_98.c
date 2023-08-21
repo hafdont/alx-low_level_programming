ab5907fb89a8e32b6754104c66fb359c672256fc
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-21 11:12:06 -0700

    Gspel og c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - function tha assigns a pinter a  value
+ *
+ * @n: variable being assigned the value asa a pointer
+ *
+ * Descriptin: This function can be called on a value to
+ * assign it as a pointe value
+ *
+ * Return: 'n' (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

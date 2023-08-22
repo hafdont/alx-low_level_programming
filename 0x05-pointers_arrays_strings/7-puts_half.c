51b811a3904e6bf74a5e642c70d9c7a354fa9218
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-22 12:00:35 -0700

    Gospel of C

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,6 +4,25 @@
 /**
  * puts_half - Prints half a string
  *
- * @str : pointers to the string 
- *
- *
+ * @str : pointers to the string
+ * Reurn: void
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+	int start;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start = (length - 1) / 2;
+
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

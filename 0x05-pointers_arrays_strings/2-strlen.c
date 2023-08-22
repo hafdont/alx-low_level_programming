f5144682edbecbd209c8c1d09396be736f588327
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-22 08:24:43 -0700

    Lets go

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen: this function returns the length of a string
+ * _strlen - this function returns the length of a string
  *
  * Description: This functiin works by increasing the pointer untill
  * it reaches the null terminator

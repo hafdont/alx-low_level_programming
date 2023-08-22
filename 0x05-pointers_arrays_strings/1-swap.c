91f947da2fe61e17863c897e837bb7aeb6b73fb4
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-21 19:01:32 -0700

    Gospel of C

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,10 +2,11 @@
 
 /**
  * swap_int - a function that swaps the value of two intergers
- * @a, @b:  thw tw integers whose values are swapped 
+ * @a: one of the two variables being swapped
+ * @b: one of the two integers whose values are swapped
  *
- * Desription: this prgram works b first string aintegers vlue 
- * in a temprary hlding place before replacing it 
+ * Desription: this prgram works b first string aintegers vlue
+ * in a temprary hlding place before replacing it
  *
  * Return: Void
  */

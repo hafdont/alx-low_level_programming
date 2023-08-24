a9e4342f4fcb97ccbc37140cb434cd4a97397084
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-24 13:54:01 -0700

    Gospel of C

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);

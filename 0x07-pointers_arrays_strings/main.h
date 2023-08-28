98fa9690c77321c085dad8eb24aa5c3ba6725403
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-08-28 16:55:00 -0700

    play chess

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);

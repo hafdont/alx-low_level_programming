f35e1e50e81a5a058ae912ad19585f89cba594ee
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-09-02 19:27:23 -0700

    The arguments and command line business

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -_Entry point
+ * @argc: The number of argumnets
+ * @argv: An array containing the arguments
+ *
+ * Description: THis is the ghetto
+ *
+ * Return: 0 always (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+
+}

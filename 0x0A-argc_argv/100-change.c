3a812beacc2186d1a169a08bfd561c42046698e5
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-09-03 07:27:56 -0700

    Killed the last one

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: an aray of the arguments
+ *
+ * Return: 1 if args arent two, 0 on succes
+ */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return (1);
-    }
 
-
-    int cents = atoi(arg[i]);
-    if (cents < 0)
-        {
-            printf("0\n");
-	    
-	    return(0);
+	int cents;
+	int coins[]= {25,10, 5, 2, 1 };
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int i,count = 0;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
 	}
-    int coins[] = {25, 10, 5, 2, 1};
-    int num_coins = sizeof(coins) / sizeof(coins[0]);
-    int min_coins = 0;
-    
-    for (i = 0; i < num_coins; i++)
-    {
-	    while (cents >= coins[i])
-	    {
-		    cents -= coins[i];
-		    min_coins++;
-	    }
-    }
-    printf("%d\n", min_coins);
 
-    return (0);
+	cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	
+	
+	for (i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			count++;
+		}
+	}
+	printf("%d\n", count);
+	return (0);
 }

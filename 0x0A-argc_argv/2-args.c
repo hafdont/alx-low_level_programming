c7902b64287c7d719e9071b9f54693ccf3fc6e13
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-09-02 20:00:38 -0700

    late again

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: The number of arguments
+ * @argv: An array containing arguments
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return(0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Thenumber of arguments
+ * @argv: An array containing the arguments
+ *
+ * Return: 0 on successs, 1 0n error
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}

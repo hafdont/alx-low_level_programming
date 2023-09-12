9a68af84a37910bb2bcf66b4770db3438221e7e3
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-09-11 18:50:38 -0700

    Hafdonte niggah!

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO_H  
+#ifndef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO_H */
+#endif
---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Prints the nameof the base file
+ *
+ * Return: Void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

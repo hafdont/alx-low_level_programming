9c6695f6e48d3abcd6ecdf59612e1e09795df6f6
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-09-18 08:46:21 -0700

    Dogs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,8 +15,4 @@ struct dog
 	char *owner;
 };
 
-dog_t *new_dog(char *name, float age, char *owner);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 #endif /*DOG_H */

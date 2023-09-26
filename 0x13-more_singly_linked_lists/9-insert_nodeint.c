95f01d1bfdbb7a6db7598070cd1f0882a6bc13a9
Hafiz Yusuf <hafizmasoud7@gmail.com>
2023-09-26 16:24:55 -0700

    JAAY

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,16 +7,16 @@
  * @head: Pointer to the pointer to the head of the list
  * @idx: index where the new node sould be added
  * @n: THe value to be inserted in the new node
- * 
+ *
  * Return: THe address of the new node.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint *new_node, *current;
+	listint_t *new_node, *current;
 	unsigned int i = 0;
 
-	if(head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -33,10 +33,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
-	listint_t *current = *head;
+	current = *head;
+
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (current == NULL)

1b9f7a6753c129007db42b21a868293f52dab183
Hafiz Yusuf <hafizmasoud7@gmail.com>
2024-04-11 07:26:06 +0300

    Task 1

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+/* search_algos.h */
+
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdio.h> /* for printf */
+#include <stdlib.h> /* for size_t */
+
+int linear_search(int *array, size_t size, int value);
+#endif /* SEARCH_ALGOS_H */
